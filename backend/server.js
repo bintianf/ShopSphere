c2594371d8fe4e1f9619b1e7da43e472a405c561
bintianf <bintianf@gmail.com>
2024-04-12 15:25:23 -0400

    backend and database connected

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+
+const port = process.env.PORT || 5000;
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running....');
+});
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
